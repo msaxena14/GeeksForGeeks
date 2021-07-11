495f89b494cb27778c9ccbb20d8e4040e61d597e
Madhavam Saxena <68888933+msaxena14@users.noreply.github.com>
2021-07-11 18:14:51 +0530

    Create Check if a string is Isogram or not.cpp

---
+++ Basic/Check if a string is Isogram or not.cpp
@@ -0,0 +1,43 @@
+#include<bits/stdc++.h>
+using namespace std;
+
+
+ // } Driver Code Ends
+
+
+class Solution
+{
+    public:
+    //Function to check if a string is Isogram or not.
+    bool isIsogram(string s)
+    {
+        //Your code here
+        unordered_map<char,int> ump;
+        for(auto ele : s){
+            ump[ele]++;
+        }
+        for(auto ele : ump){
+            if(ele.second > 1)
+                return false;
+        }
+        return true;
+    }
+};
+
+// { Driver Code Starts.
+
+int main()
+ {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        
+        string s;
+        cin>>s;
+        Solution obj;
+        cout<<obj.isIsogram(s)<<endl;
+        
+    }
+	return 0;
+}  // } Driver Code Ends
