f1044f65fe33818618539abada325a6536e4a37a
Madhavam Saxena <68888933+msaxena14@users.noreply.github.com>
2021-07-11 18:04:09 +0530

    Create Searching an element in a sorted array.cpp

---
+++ Basic/Searching an element in a sorted array.cpp
@@ -0,0 +1,50 @@
+#include <bits/stdc++.h> 
+using namespace std;
+
+
+ // } Driver Code Ends
+class Solution{
+    public:
+    // Function to find element in sorted array
+    // arr: input array
+    // N: size of array
+    // K: element to be searche
+    int searchInSorted(int arr[], int N, int K) 
+    { 
+        int no = binary_search(arr, arr+N, K);
+        if(no == 1){
+            return no;
+        }
+        else{
+            no = -1;
+            return no;
+        }
+    }
+};
+
+// { Driver Code Starts.
+
+
+int main(void) 
+{ 
+    
+    int t;
+    cin >> t;
+    while(t--){
+        int n, k;
+        cin >> n >> k;
+        
+        int arr[n];
+        
+        for(int i = 0;i<n;i++){
+            cin >> arr[i];
+        }
+        
+        Solution ob;
+        cout << ob.searchInSorted(arr, n, k) << endl;
+
+    }
+
+	return 0; 
+} 
+  // } Driver Code Ends
