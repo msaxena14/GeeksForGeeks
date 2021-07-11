cf181b2c9f88ca4f97e679cb55809b3648379f3e
Madhavam Saxena <68888933+msaxena14@users.noreply.github.com>
2021-07-11 18:02:17 +0530

    Create Smallest divisible number .cpp

---
+++ Basic/Smallest divisible number .cpp
@@ -0,0 +1,33 @@
+//Initial template for C++
+
+#include <bits/stdc++.h>
+using namespace std;
+
+ // } Driver Code Ends
+//User function template for C++
+
+class Solution{
+public:
+
+    long long getSmallestDivNum(long long n){
+        long long res = 1;
+        for(long long i=1; i<=n; ++i){
+            res = (res * i) / __gcd(res, i);
+        }
+        return res;
+    }
+};
+
+// { Driver Code Starts.
+int main() {
+    int t;
+    cin>>t;
+    while(t--)
+    {
+        int n;
+        cin>>n;
+        Solution ob;
+        cout<< ob.getSmallestDivNum(n)<<endl;
+    }
+    return 0;
+}  // } Driver Code Ends
